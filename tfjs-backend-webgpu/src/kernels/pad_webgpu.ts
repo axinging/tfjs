11595ce5cab0e07deccbe3f371ca155f5ed6e39d
Xu Xing <xing.xu@intel.com>
2020-11-02 20:42:22 +0800

    Pad works under WPT 8

--- tfjs-backend-webgpu/src/kernels/pad_webgpu.ts
+++ tfjs-backend-webgpu/src/kernels/pad_webgpu.ts
@@ -31,8 +31,7 @@ export class PadProgram implements WebGPUProgram {
   // TODO(texture).
   variableNames: string[] = [];
   variableTextureNames = ['x'];
-  // TODO(texture): make this works under WPT = 8.
-  workPerThread = 1;
+  workPerThread = 8;
   workGroupSize: [number, number, number] = [16, 1, 1];
 
   constructor(
@@ -53,12 +52,11 @@ export class PadProgram implements WebGPUProgram {
     const end = paddings.map((p, i) => p[0] + xShape[i]).join(',');
     const startValue = rank > 1 ? `${type}(${start})` : `${start}`;
     const endValue = rank > 1 ? `${type}(${end})` : `${end}`;
-    const inShape = xShape;
 
     const leftPadCondition =
-        rank > 1 ? `any(lessThan(outC, start))` : `outC < start`;
+        rank > 1 ? `any(lessThan(outCoords, start))` : `outCoords < start`;
     const rightPadCondition =
-        rank > 1 ? `any(greaterThanEqual(outC, end))` : `outC >= end`;
+        rank > 1 ? `any(greaterThanEqual(outCoords, end))` : `outCoords >= end`;
 
     const unpackedCoords = rank > 1 ?
         ['coords[0]', 'coords[1]', 'coords[2]', 'coords[3]'].slice(0, rank) :
@@ -67,50 +65,28 @@ export class PadProgram implements WebGPUProgram {
     const dims = ['coords[0]', 'coords[1]', 'coords[2]', 'coords[3]'].slice(
         0, this.outputShape.length);
     dims.map(d => `${d}`).join(', ');
+
+    const dims2 =
+        ['outCoords[0]', 'outCoords[1]', 'outCoords[2]', 'outCoords[3]'].slice(
+            0, this.outputShape.length);
+    dims2.map(d => `${d}`).join(', ');
     this.userCode = `
       ${type} start = ${startValue};
       ${type} end = ${endValue};
 
       void main() {
         int index = int(gl_GlobalInvocationID.x);
-
         for (int i = 0; i < ${this.workPerThread}; i++) {
           int flatIndex = index * ${this.workPerThread} + i;
-
           if (flatIndex < ${size}) {
-            ${type} outC = getCoordsFromFlatIndex(flatIndex);
+            ${type} outCoords = getCoordsFromFlatIndex(flatIndex);
 
             if (${leftPadCondition} || ${rightPadCondition}) {
-			  ${type} coords = outC;
+			  ${type} coords = outCoords;
               setOutput(${dims}, ${constantValue});
             } else {
-              ${type} coords = outC - start;
-              //setOutput(flatIndex, getX(${unpackedCoords}));
-              // setOutput(${dims}, getX(${unpackedCoords}));
-              setOutput(getX(${unpackedCoords}));
-              int texR, texC;
-			  /*
-
-              texR = int(dot(vec3(coords[0], coords[1], coords[2]), vec3(${
-        inShape[1]} * ${inShape[2]}, ${inShape[2]}, 1)) );
-              texC = coords[3];
-    
-              float value = imageLoad(x, ivec2(texC,texR)).r;
-              */
-              /*
-              int texR2 = int(dot(vec3(outCoord[0], outCoord[1], outCoord[2]), vec3(${
-        this.outputShape[1]} * ${this.outputShape[2]}, ${
-        this.outputShape[2]}, 1)) );
-              int texC2 = outCoord[3];
-              imageStore(result, ivec2(texC2,texR2), vec4(value, 0.0, 0.0, 0.0));
-              */
-             /*
-              ivec2 uv = uvFromFlat(9, 5, flatIndex); // padtexture0d1 works.
-              //ivec2 uv = getUVFromFlat(flatIndex);
-              //ivec2 uv = uvFromFlat(121, 3, flatIndex); // padtexture0d2 works?.
-              imageStore(result, ivec2(uv.y,uv.x), vec4(value, 0.0, 0.0, 0.0));
-              */
-              //
+              ${type} coords = outCoords - start;
+              setOutput(${dims2},  getX(${unpackedCoords}));
             }
           }
         }
